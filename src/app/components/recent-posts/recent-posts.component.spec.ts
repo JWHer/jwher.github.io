17df5863ddfabd817926a7727cbdcec74817ed7c
JeongWon Her <hacel@naver.com>
2022-03-16 01:21:04 +0900

    add recent-posts

---
+++ src/app/components/recent-posts/recent-posts.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecentPostsComponent } from './recent-posts.component';
+
+describe('RecentPostsComponent', () => {
+  let component: RecentPostsComponent;
+  let fixture: ComponentFixture<RecentPostsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RecentPostsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RecentPostsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
