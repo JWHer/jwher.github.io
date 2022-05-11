47d344e8e6a0c38858bff0c9d17831b38135f93f
JeongWon Her <hacel@naver.com>
2022-05-11 23:42:20 +0900

    instagram api test

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  data: {media_url:string, caption:string}[]=[];
+  constructor() {
   }
-
+  
+  ngOnInit(): void {
+    (
+      async () => {
+        const response = await fetch('/api/me/media?fields=id,caption&access_token=IGQVJVNW5TbldjdWo5NkhCeDRvOUFLUU55a2xsbUtJSXllaUFZAYmozUUFoNnNERUZACOXpENG9kUFMwcGlHd3B4YnpheEV3ODRrcFA0QVpoa1ZApOVpleGlNVUlZAU1ZAMQ21ZAbGRvdy1nOVdMQW9ZAQWhDTAZDZD');
+        let mediaList: {data:Array<{id:string, caption:string}>} = await response.json();
+        console.log(mediaList);
+        for(const media of mediaList.data){
+          // console.log("media:"+media);
+          const response = await fetch('/api/'+media.id+'?fields=media_type,media_url&access_token=IGQVJVNW5TbldjdWo5NkhCeDRvOUFLUU55a2xsbUtJSXllaUFZAYmozUUFoNnNERUZACOXpENG9kUFMwcGlHd3B4YnpheEV3ODRrcFA0QVpoa1ZApOVpleGlNVUlZAU1ZAMQ21ZAbGRvdy1nOVdMQW9ZAQWhDTAZDZD');
+          let result:{media_type:string, media_url:string} = await response.json();
+          console.log(result);
+          if(result.media_type=='IMAGE'){
+            this.data.push({media_url:result.media_url, caption:media.caption});
+          }
+        }
+        console.log(this.data);
+      }
+    )();
+      
+  }
 }
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -24,6 +25,7 @@ import { CommentsComponent } from './comments/comments.component';
     CommentsComponent,
   ],
   imports: [
+    CommonModule,
     FontAwesomeModule,
     NgbModule,
   ],
