95fdb995a30340ebcf2318ee3bcd0008bc75182f
JeongWon Her <hacel@naver.com>
2022-05-12 23:46:50 +0900

    update posts page

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.scss']
 })
 export class PostsComponent implements OnInit {
-
+  page:{url:string, title:string, desc:string, thumbnail:string} = {
+    url: "https://jwher.github.io",
+    title: "test",
+    desc: "description",
+    thumbnail: "https://images.pexels.com/photos/1591447/pexels-photo-1591447.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  }
   constructor() { }
 
   ngOnInit(): void {
