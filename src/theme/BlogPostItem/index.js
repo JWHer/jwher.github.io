aacd2580d644afa1cb98b7accc3395d1bc97f9b7
jwher <hacel@naver.com>
2022-06-06 19:26:23 +0900

    add utterances & buy me a coffee

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import clsx from 'clsx';
 import Translate, {translate} from '@docusaurus/Translate';
 import Link from '@docusaurus/Link';
 import {useBaseUrlUtils} from '@docusaurus/useBaseUrl';
 import {usePluralForm} from '@docusaurus/theme-common';
 import {blogPostContainerID} from '@docusaurus/utils-common';
+import { useColorMode } from "@docusaurus/theme-common";
 import MDXContent from '@theme/MDXContent';
 import EditThisPage from '@theme/EditThisPage';
 import styles from './styles.module.css';
 import TagsListInline from '@theme/TagsListInline';
 import BlogPostAuthors from '@theme/BlogPostAuthors'; // Very simple pluralization: probably good enough for now
+
+const utterancesSelector = "iframe.utterances-frame";
 
 function useReadingTimePlural() {
   const {selectMessage} = usePluralForm();
@@ -57,6 +60,43 @@ export default function BlogPostItem(props) {
   const truncatedPost = !isBlogPostPage && truncated;
   const tagsExists = tags.length > 0;
   const TitleHeading = isBlogPostPage ? 'h1' : 'h2';
+
+  // utterances
+  const { isDarkTheme } = useColorMode();
+  const utterancesTheme = isDarkTheme ? "github-dark" : "github-light";
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (!props.isBlogPostPage) return;
+
+    const utterancesEl = containerRef.current.querySelector(utterancesSelector);
+
+    const createUtterancesEl = () => {
+      const script = document.createElement("script");
+
+      script.src = "https://utteranc.es/client.js";
+      script.setAttribute("repo", "JWHer/JWHer");
+      script.setAttribute("issue-term", "title");
+      script.setAttribute("label", "comment");
+      script.setAttribute("theme", utterancesTheme);
+      script.crossOrigin = "anonymous";
+      script.async = true;
+
+      containerRef.current.appendChild(script);
+    };
+
+    const postThemeMessage = () => {
+      const message = {
+        type: "set-theme",
+        theme: utterancesTheme,
+      };
+
+      utterancesEl.contentWindow.postMessage(message, "https://utteranc.es");
+    };
+
+    utterancesEl ? postThemeMessage() : createUtterancesEl();
+  }, [utterancesTheme]);
+
   return (
     <article
       className={!isBlogPostPage ? 'margin-bottom--xl' : undefined}
@@ -103,6 +143,10 @@ export default function BlogPostItem(props) {
         itemProp="articleBody">
         <MDXContent>{children}</MDXContent>
       </div>
+
+      <a href="https://www.buymeacoffee.com/jwher">
+        <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=jwher&button_colour=40DCA5&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" />
+      </a>
 
       {(tagsExists || truncated) && (
         <footer
@@ -155,6 +199,7 @@ export default function BlogPostItem(props) {
           )}
         </footer>
       )}
+      {props.isBlogPostPage && <div ref={containerRef} />}
     </article>
   );
 }
