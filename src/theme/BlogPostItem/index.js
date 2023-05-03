a8f338de8b4a2945ccdc614873d89695030ccba1
jwher <hacel@naver.com>
2023-05-03 19:31:31 +0900

    update docusaurus 2.4.0

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -1,65 +1,30 @@
 import React, { useEffect, useRef } from 'react';
 import clsx from 'clsx';
-import Translate, {translate} from '@docusaurus/Translate';
-import Link from '@docusaurus/Link';
-import {useBaseUrlUtils} from '@docusaurus/useBaseUrl';
-import {usePluralForm} from '@docusaurus/theme-common';
-import {blogPostContainerID} from '@docusaurus/utils-common';
-import { useColorMode } from "@docusaurus/theme-common";
-import MDXContent from '@theme/MDXContent';
-import EditThisPage from '@theme/EditThisPage';
-import styles from './styles.module.css';
-import TagsListInline from '@theme/TagsListInline';
-import BlogPostAuthors from '@theme/BlogPostAuthors'; // Very simple pluralization: probably good enough for now
+import {useColorMode} from "@docusaurus/theme-common";
+import {useBlogPost} from '@docusaurus/theme-common/internal';
+import BlogPostItemContainer from '@theme/BlogPostItem/Container';
+import BlogPostItemHeader from '@theme/BlogPostItem/Header';
+import BlogPostItemContent from '@theme/BlogPostItem/Content';
+import BlogPostItemFooter from '@theme/BlogPostItem/Footer';
 
 const utterancesSelector = "iframe.utterances-frame";
 
-function useReadingTimePlural() {
-  const {selectMessage} = usePluralForm();
-  return (readingTimeFloat) => {
-    const readingTime = Math.ceil(readingTimeFloat);
-    return selectMessage(
-      readingTime,
-      translate(
-        {
-          id: 'theme.blog.post.readingTime.plurals',
-          description:
-            'Pluralized label for "{readingTime} min read". Use as much plural forms (separated by "|") as your language support (see https://www.unicode.org/cldr/cldr-aux/charts/34/supplemental/language_plural_rules.html)',
-          message: 'One min read|{readingTime} min read',
-        },
-        {
-          readingTime,
-        },
-      ),
-    );
-  };
+// apply a bottom margin in list view
+function useContainerClassName() {
+  const {isBlogPostPage} = useBlogPost();
+  return !isBlogPostPage ? 'margin-bottom--xl' : undefined;
 }
 
-export default function BlogPostItem(props) {
-  const readingTimePlural = useReadingTimePlural();
-  const {withBaseUrl} = useBaseUrlUtils();
+export default function BlogPostItem({children, className}) {
+  const containerClassName = useContainerClassName();
   const {
-    children,
-    frontMatter,
-    assets,
     metadata,
-    truncated,
     isBlogPostPage = false,
-  } = props;
+  } = useBlogPost();
   const {
-    date,
-    formattedDate,
-    permalink,
-    tags,
-    readingTime,
-    title,
-    editUrl,
-    authors,
+    hasTruncateMarker
   } = metadata;
-  const image = assets.image ?? frontMatter.image;
-  const truncatedPost = !isBlogPostPage && truncated;
-  const tagsExists = tags.length > 0;
-  const TitleHeading = isBlogPostPage ? 'h1' : 'h2';
+  const truncatedPost = !isBlogPostPage && hasTruncateMarker;
 
   // utterances
   const { isDarkTheme } = useColorMode();
@@ -67,7 +32,7 @@ export default function BlogPostItem(props) {
   const containerRef = useRef(null);
 
   useEffect(() => {
-    if (!props.isBlogPostPage) return;
+    if (!isBlogPostPage) return;
 
     const utterancesEl = containerRef.current.querySelector(utterancesSelector);
 
@@ -98,111 +63,17 @@ export default function BlogPostItem(props) {
   }, [utterancesTheme]);
 
   return (
-    <article
-      className={!isBlogPostPage ? 'margin-bottom--xl' : undefined}
-      itemProp="blogPost"
-      itemScope
-      itemType="http://schema.org/BlogPosting">
-      <header>
-        <TitleHeading className={styles.blogPostTitle} itemProp="headline">
-          {isBlogPostPage ? (
-            title
-          ) : (
-            <Link itemProp="url" to={permalink}>
-              {title}
-            </Link>
-          )}
-        </TitleHeading>
-        <div className={clsx(styles.blogPostData, 'margin-vert--md')}>
-          <time dateTime={date} itemProp="datePublished">
-            {formattedDate}
-          </time>
-
-          {typeof readingTime !== 'undefined' && (
-            <>
-              {' · '}
-              {readingTimePlural(readingTime)}
-            </>
-          )}
-        </div>
-        <BlogPostAuthors authors={authors} assets={assets} />
-      </header>
-
-      {image && (
-        <meta
-          itemProp="image"
-          content={withBaseUrl(image, {
-            absolute: true,
-          })}
-        />
-      )}
-
-      <div // This ID is used for the feed generation to locate the main content
-        id={isBlogPostPage ? blogPostContainerID : undefined}
-        className="markdown"
-        itemProp="articleBody">
-        <MDXContent>{children}</MDXContent>
-      </div>
-
+    <BlogPostItemContainer className={clsx(containerClassName, className)}>
+      <BlogPostItemHeader />
+      <BlogPostItemContent>{children}</BlogPostItemContent>
       {isBlogPostPage ?
       <a href="https://www.buymeacoffee.com/jwher">
         <img style={{margin: "20px 0"}}
           src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=jwher&button_colour=40DCA5&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" />
       </a>:<></>
       }
-
-      {(tagsExists || truncated) && (
-        <footer
-          className={clsx(
-            'row docusaurus-mt-lg',
-            isBlogPostPage && styles.blogPostDetailsFull,
-          )}>
-          {tagsExists && (
-            <div
-              className={clsx('col', {
-                'col--9': truncatedPost,
-              })}>
-              <TagsListInline tags={tags} />
-            </div>
-          )}
-
-          {isBlogPostPage && editUrl && (
-            <div className="col margin-top--sm">
-              <EditThisPage editUrl={editUrl} />
-            </div>
-          )}
-
-          {truncatedPost && (
-            <div
-              className={clsx('col text--right', {
-                'col--3': tagsExists,
-              })}>
-              <Link
-                to={metadata.permalink}
-                aria-label={translate(
-                  {
-                    message: 'Read more about {title}',
-                    id: 'theme.blog.post.readMoreLabel',
-                    description:
-                      'The ARIA label for the link to full blog posts from excerpts',
-                  },
-                  {
-                    title,
-                  },
-                )}>
-                <b>
-                  <Translate
-                    id="theme.blog.post.readMore"
-                    description="The label used in blog post item excerpts to link to full blog posts">
-                    Read More
-                  </Translate>
-                </b>
-              </Link>
-            </div>
-          )}
-        </footer>
-      )}
-      {props.isBlogPostPage && <div ref={containerRef} />}
-    </article>
+      <BlogPostItemFooter />
+      {isBlogPostPage && <div ref={containerRef} />}
+    </BlogPostItemContainer>
   );
 }
--- src/theme/DocPage/Layout/index.js
+++ src/theme/DocPage/Layout/index.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
+import {useDocsSidebar} from '@docusaurus/theme-common/internal';
 import Layout from '@theme/Layout';
 import BackToTopButton from '@theme/BackToTopButton';
 import DocPageLayoutSidebar from '@theme/DocPage/Layout/Sidebar';
 import DocPageLayoutMain from '@theme/DocPage/Layout/Main';
-import styles from "./styles.module.scss";
-import {useDocsSidebar} from '@docusaurus/theme-common';
+import styles from './styles.module.scss';
+
 export default function DocPageLayout({children}) {
   const sidebar = useDocsSidebar();
   const [hiddenSidebarContainer, setHiddenSidebarContainer] = useState(false);
